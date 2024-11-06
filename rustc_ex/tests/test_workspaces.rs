67d84695b580d89d63bb317de6be1203279d3c06
Luca Favini <luca.favini.02@gmail.com>
2024-11-06 14:45:31 +0100

    update workspaces tests

--- rustc_ex/tests/test_workspaces.rs
+++ rustc_ex/tests/test_workspaces.rs
@@ -26,55 +26,23 @@ fn test_help_output() -> Result<(), String> {
 }
 
 #[test]
-#[ignore] // TODO: test grafo degli artefatti dopo averlo definito bene
-fn test_simple_feature_no_weigths_artifacts_dot() -> Result<(), String> {
+fn test_simple_feature_no_weigths_features_graph() -> Result<(), String> {
     let (output, _) = run_with_cargo_bin(
         "workspaces/simple_feature_no_weights",
         None,
-        &["--print-artifacts-dot"],
+        &["--print-features-graph"],
     )?;
 
-    assert!(output.contains(
-        r#"digraph {
-    0 [ label="__GLOBAL__ #[__GLOBAL__]"]
-    1 [ label="one #[aa]"]
-    2 [ label="two #[not(bb)]"]
-    3 [ label="three #[cc]"]
-    4 [ label="four #[dd]"]
-    5 [ label="five #[ee]"]
-    6 [ label="six #[not(ff)]"]
-    1 -> 0 [ label="0"]
-    2 -> 0 [ label="0"]
-    5 -> 4 [ label="0"]
-    6 -> 4 [ label="0"]
-    4 -> 3 [ label="0"]
-    3 -> 0 [ label="0"]
-}"#
-    ));
+    // nodes
+    assert!(output.contains("0 [ label=\"__GLOBAL__\"]"));
+    assert!(output.contains("1 [ label=\"aa\"]"));
+    assert!(output.contains("2 [ label=\"!bb\"]"));
+    assert!(output.contains("3 [ label=\"cc\"]"));
+    assert!(output.contains("4 [ label=\"dd\"]"));
+    assert!(output.contains("5 [ label=\"ee\"]"));
+    assert!(output.contains("6 [ label=\"!ff\"]"));
 
-    Ok(())
-}
-
-#[test]
-fn test_simple_feature_no_weigths_features_dot() -> Result<(), String> {
-    let (output, _) = run_with_cargo_bin(
-        "workspaces/simple_feature_no_weights",
-        None,
-        &["--print-features-dot"],
-    )?;
-
-    assert!(output.contains(
-        r#"digraph {
-    0 [ label="__GLOBAL__"]
-    1 [ label="aa"]
-    2 [ label="!bb"]
-    3 [ label="cc"]
-    4 [ label="dd"]
-    5 [ label="ee"]
-    6 [ label="!ff"]"#
-    ));
-
-    // edges order is not deterministic
+    // edges
     assert!(output.contains("5 -> 4 [ label=\"1.00\"]"));
     assert!(output.contains("4 -> 3 [ label=\"1.00\"]"));
     assert!(output.contains("6 -> 4 [ label=\"1.00\"]"));
@@ -86,51 +54,23 @@ fn test_simple_feature_no_weigths_features_dot() -> Result<(), String> {
 }
 
 #[test]
-#[ignore] // TODO: test grafo degli artefatti dopo averlo definito bene
-fn test_simple_feature_weigths_artifacts_dot() -> Result<(), String> {
+fn test_simple_feature_weigths_features_graph() -> Result<(), String> {
     let (output, _) = run_with_cargo_bin(
         "workspaces/simple_feature_weights",
         None,
-        &["--print-artifacts-dot"],
+        &["--print-features-graph"],
     )?;
 
-    assert!(output.contains(
-        r#"digraph {
-    0 [ label="__GLOBAL__ #[__GLOBAL__]"]
-    1 [ label="one #[aa]"]
-    2 [ label="two #[any(bb, cc)]"]
-    3 [ label="three #[all(ee, not(ff))]"]
-    4 [ label="four #[dd]"]
-    2 -> 1 [ label="0"]
-    1 -> 0 [ label="0"]
-    4 -> 3 [ label="0"]
-    3 -> 0 [ label="0"]
-}"#
-    ));
+    // nodes
+    assert!(output.contains("0 [ label=\"__GLOBAL__\"]"));
+    assert!(output.contains("1 [ label=\"aa\"]"));
+    assert!(output.contains("2 [ label=\"bb\"]"));
+    assert!(output.contains("3 [ label=\"cc\"]"));
+    assert!(output.contains("4 [ label=\"ee\"]"));
+    assert!(output.contains("5 [ label=\"!ff\"]"));
+    assert!(output.contains("6 [ label=\"dd\"]"));
 
-    Ok(())
-}
-
-#[test]
-fn test_simple_feature_weigths_features_dot() -> Result<(), String> {
-    let (output, _) = run_with_cargo_bin(
-        "workspaces/simple_feature_weights",
-        None,
-        &["--print-features-dot"],
-    )?;
-
-    assert!(output.contains(
-        r#"digraph {
-    0 [ label="__GLOBAL__"]
-    1 [ label="aa"]
-    2 [ label="bb"]
-    3 [ label="cc"]
-    4 [ label="ee"]
-    5 [ label="!ff"]
-    6 [ label="dd"]"#
-    ));
-
-    // edges order is not deterministic
+    // edges
     assert!(output.contains("2 -> 1 [ label=\"1.00\"]"));
     assert!(output.contains("3 -> 1 [ label=\"1.00\"]"));
     assert!(output.contains("1 -> 0 [ label=\"1.00\"]"));
@@ -141,3 +81,6 @@ fn test_simple_feature_weigths_features_dot() -> Result<(), String> {
 
     Ok(())
 }
+
+// TODO: testare grafo degli artefatti per no weights
+// TODO: testare grafo degli artefatti per weights
