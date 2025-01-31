6a504baacb3f67b9e4729ac45d4ed294eeabb80e
Luca Favini <luca.favini.02@gmail.com>
2025-01-31 11:39:58 +0100

    fix tests broken due to weight tweaks

--- rustc_ex/tests/test_snippets_call_weights.rs
+++ rustc_ex/tests/test_snippets_call_weights.rs
@@ -12,7 +12,7 @@ fn test_call_to_after() -> Result<(), String> {
     let snippet = &std::fs::read_to_string(format!("{FOLDER}/call_to_after.rs")).unwrap();
     let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-ast-graph"])?;
 
-    assert!(same_line(&output, vec!["Fn", "'a'", "w2.00"]));
+    assert!(same_line(&output, vec!["Fn", "'a'", "w1.00"]));
     assert!(same_line(&output, vec!["Call(Call)->a", "w2.00"]));
 
     Ok(())
@@ -23,7 +23,7 @@ fn test_call_to_before() -> Result<(), String> {
     let snippet = &std::fs::read_to_string(format!("{FOLDER}/call_to_before.rs")).unwrap();
     let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-ast-graph"])?;
 
-    assert!(same_line(&output, vec!["Fn", "'a'", "w2.00"]));
+    assert!(same_line(&output, vec!["Fn", "'a'", "w1.00"]));
     assert!(same_line(&output, vec!["Call(Call)->a", "w2.00"]));
 
     Ok(())
@@ -34,7 +34,7 @@ fn test_call_to_inner_after() -> Result<(), String> {
     let snippet = &std::fs::read_to_string(format!("{FOLDER}/call_to_inner_after.rs")).unwrap();
     let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-ast-graph"])?;
 
-    assert!(same_line(&output, vec!["Fn", "'a'", "w2.00"]));
+    assert!(same_line(&output, vec!["Fn", "'a'", "w1.00"]));
     assert!(same_line(&output, vec!["Call(Call)->a", "w2.00"]));
 
     Ok(())
@@ -45,7 +45,7 @@ fn test_call_to_inner_before() -> Result<(), String> {
     let snippet = &std::fs::read_to_string(format!("{FOLDER}/call_to_inner_before.rs")).unwrap();
     let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-ast-graph"])?;
 
-    assert!(same_line(&output, vec!["Fn", "'a'", "w2.00"]));
+    assert!(same_line(&output, vec!["Fn", "'a'", "w1.00"]));
     assert!(same_line(&output, vec!["Call(Call)->a", "w2.00"]));
 
     Ok(())
@@ -56,9 +56,9 @@ fn test_double_call1() -> Result<(), String> {
     let snippet = &std::fs::read_to_string(format!("{FOLDER}/double_call1.rs")).unwrap();
     let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-ast-graph"])?;
 
-    assert!(same_line(&output, vec!["Fn", "'b'", "w2.00"]));
+    assert!(same_line(&output, vec!["Fn", "'b'", "w1.00"]));
     assert!(same_line(&output, vec!["Call(Call)->b", "w2.00"]));
-    assert!(same_line(&output, vec!["Fn", "'a'", "w3.00"]));
+    assert!(same_line(&output, vec!["Fn", "'a'", "w2.00"]));
     assert!(same_line(&output, vec!["Call(Call)->a", "w3.00"]));
 
     Ok(())
@@ -69,9 +69,9 @@ fn test_double_call2() -> Result<(), String> {
     let snippet = &std::fs::read_to_string(format!("{FOLDER}/double_call2.rs")).unwrap();
     let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-ast-graph"])?;
 
-    assert!(same_line(&output, vec!["Fn", "'b'", "w2.00"]));
+    assert!(same_line(&output, vec!["Fn", "'b'", "w1.00"]));
     assert!(same_line(&output, vec!["Call(Call)->b", "w2.00"]));
-    assert!(same_line(&output, vec!["Fn", "'a'", "w3.00"]));
+    assert!(same_line(&output, vec!["Fn", "'a'", "w2.00"]));
     assert!(same_line(&output, vec!["Call(Call)->a", "w3.00"]));
 
     Ok(())
@@ -82,12 +82,12 @@ fn test_multiple_calls1() -> Result<(), String> {
     let snippet = &std::fs::read_to_string(format!("{FOLDER}/multiple_calls1.rs")).unwrap();
     let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-ast-graph"])?;
 
-    assert!(same_line(&output, vec!["Fn", "'f'", "w2.00"]));
-    assert!(same_line(&output, vec!["Fn", "'e'", "w3.00"]));
-    assert!(same_line(&output, vec!["Fn", "'d'", "w4.00"]));
-    assert!(same_line(&output, vec!["Fn", "'c'", "w5.00"]));
-    assert!(same_line(&output, vec!["Fn", "'b'", "w6.00"]));
-    assert!(same_line(&output, vec!["Fn", "'a'", "w7.00"]));
+    assert!(same_line(&output, vec!["Fn", "'f'", "w1.00"]));
+    assert!(same_line(&output, vec!["Fn", "'e'", "w2.00"]));
+    assert!(same_line(&output, vec!["Fn", "'d'", "w3.00"]));
+    assert!(same_line(&output, vec!["Fn", "'c'", "w4.00"]));
+    assert!(same_line(&output, vec!["Fn", "'b'", "w5.00"]));
+    assert!(same_line(&output, vec!["Fn", "'a'", "w6.00"]));
 
     assert!(same_line(&output, vec!["Call(Call)->f", "w2.00"]));
     assert!(same_line(&output, vec!["Call(Call)->e", "w3.00"]));
@@ -104,12 +104,12 @@ fn test_multiple_calls2() -> Result<(), String> {
     let snippet = &std::fs::read_to_string(format!("{FOLDER}/multiple_calls2.rs")).unwrap();
     let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-ast-graph"])?;
 
-    assert!(same_line(&output, vec!["Fn", "'f'", "w2.00"]));
-    assert!(same_line(&output, vec!["Fn", "'e'", "w3.00"]));
-    assert!(same_line(&output, vec!["Fn", "'d'", "w4.00"]));
-    assert!(same_line(&output, vec!["Fn", "'c'", "w5.00"]));
-    assert!(same_line(&output, vec!["Fn", "'b'", "w6.00"]));
-    assert!(same_line(&output, vec!["Fn", "'a'", "w7.00"]));
+    assert!(same_line(&output, vec!["Fn", "'f'", "w1.00"]));
+    assert!(same_line(&output, vec!["Fn", "'e'", "w2.00"]));
+    assert!(same_line(&output, vec!["Fn", "'d'", "w3.00"]));
+    assert!(same_line(&output, vec!["Fn", "'c'", "w4.00"]));
+    assert!(same_line(&output, vec!["Fn", "'b'", "w5.00"]));
+    assert!(same_line(&output, vec!["Fn", "'a'", "w6.00"]));
 
     assert!(same_line(&output, vec!["Call(Call)->f", "w2.00"]));
     assert!(same_line(&output, vec!["Call(Call)->e", "w3.00"]));
@@ -126,9 +126,9 @@ fn test_recursive() -> Result<(), String> {
     let snippet = &std::fs::read_to_string(format!("{FOLDER}/recursive.rs")).unwrap();
     let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-ast-graph"])?;
 
-    assert!(same_line(&output, vec!["Fn", "'main'", "wait(main)"]));
+    assert!(same_line(&output, vec!["Fn", "'main'", "w8.00"])); // resolved with recovery mode
 
-    assert!(same_line(&output, vec!["Call(Call)->main", "wait"]));
+    assert!(same_line(&output, vec!["Call(Call)->main", "w8.00"]));
 
     Ok(())
 }
@@ -138,11 +138,11 @@ fn test_mutual_recursive() -> Result<(), String> {
     let snippet = &std::fs::read_to_string(format!("{FOLDER}/mutual_recursive.rs")).unwrap();
     let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-ast-graph"])?;
 
-    assert!(same_line(&output, vec!["Fn", "'main'", "wait(a)"]));
-    assert!(same_line(&output, vec!["Fn", "'a'", "wait(main)"]));
+    assert!(same_line(&output, vec!["Fn", "'main'", "w9.00"]));
+    assert!(same_line(&output, vec!["Fn", "'a'", "9.00"]));
 
-    assert!(same_line(&output, vec!["Call(Call)->a", "wait"]));
-    assert!(same_line(&output, vec!["Call(Call)->main", "wait"]));
+    assert!(same_line(&output, vec!["Call(Call)->a", "w9.00"]));
+    assert!(same_line(&output, vec!["Call(Call)->main", "w9.00"]));
 
     Ok(())
 }
