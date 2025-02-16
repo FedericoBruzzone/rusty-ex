d3cdb0aa1a583bd4c08a07ae209517396d25162a
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2025-02-16 12:09:23 +0100

    `#[ignore]` tests we expect to fail
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- rusty_ex/tests/test_snippets_detect_artifacts.rs
+++ rusty_ex/tests/test_snippets_detect_artifacts.rs
@@ -8,6 +8,7 @@ const FOLDER: &str = "tests/snippets/detect_artifacts";
 // =============================================
 
 #[test]
+#[ignore] // It is expected to fail
 fn test_macro() -> Result<(), String> {
     let snippet = &std::fs::read_to_string(format!("{FOLDER}/macro.rs")).unwrap();
     let (output, _) = run_with_cargo_bin_and_snippet(snippet, &["--print-terms-tree"])?;
